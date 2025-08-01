bdb6308b6a92127d5e2008ca20dc7c7ca64fe298
Kgomotso <kgomotso.rapodile1@gmail.com>
2025-08-01 19:25:53 +0200

    Add files via upload

--- static/js/script.js
+++ static/js/script.js
@@ -1,1 +1,18 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const form = document.getElementById("booking-form");
+    const successMessage = document.getElementById("successMessage");
 
+    form.addEventListener("submit", (e) => {
+        const name = form.name.value;
+        const phone = form.phone.value;
+
+        //Simple check: if name and phoneare too short to prevent submission
+        if (name.length < 3 || phone.length < 10) {
+            e.preventDefault(); // Prevent form submission
+            alert("Please enter a valid name and phone number.");
+            return;
+        } else {
+            successMessage.textContent = "Booking submitted successfully!"
+        }
+    });
+});
